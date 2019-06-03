541b03c25c662ea33a1de5ed6354fdfe2487cdc8
Vesa Hovinen <46774886+welmuttaja@users.noreply.github.com>
2019-06-03 13:22:32 +0300

    Monsteri
    Monteri spawni + liikkuminen

---
+++ ContraCopio/Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public float spawn = 1f;
+    public float spawnTime = 3f;
+    private Vector2 spawnPosition;
+    private Vector2 target;
+    private Rigidbody2D enemyRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Spawn()
+    {
+    spawnPosition.x = 8;
+    spawnPosition.y = 1;
+
+    Instantiate(enemy, spawnPosition, Quaternion.identity);
+    }
+}
