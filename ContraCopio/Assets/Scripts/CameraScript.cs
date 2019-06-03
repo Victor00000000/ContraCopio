9fc0e28b9f5563dff847070f4db34e0f3848b661
Viktor Haimi <viktor.i.haimi@gmail.com>
2019-06-03 12:57:45 +0300

    player movement
    movement

---
+++ ContraCopio/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.transform.position.x > transform.position.x)
+        {
+            transform.position = new Vector3(target.transform.position.x, transform.position.y, -10f);
+        }
+
+        if (target.transform.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.transform.position.y, -10f);
+        }
+
+        if (target.transform.position.y < transform.position.y && transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, target.transform.position.y, -10f);
+        }
+    }
+}
--- ContraCopio/Assets/Scripts/PlayerScript.cs
+++ ContraCopio/Assets/Scripts/PlayerScript.cs
@@ -6,21 +6,68 @@ public class PlayerScript : MonoBehaviour
 {
 
     Vector2 inputVector;
+    public float speed;
+    public float jumpingSpeed;
+    Rigidbody2D rb2d;
+    Animator animator;
+    bool jumping = false;
+    bool sit = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputReader();
+        Move();
+
+        if (animator.GetBool("Jumping") && (rb2d.velocity.sqrMagnitude < 0.0001f))
+        {
+            jumping = false;
+            animator.SetBool("Jumping", false);
+        }
     }
 
-    void UpdateInputVector()
+    void InputReader()
     {
-       
+        inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+            transform.localScale = new Vector3(1, 1, 1);
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && jumping == false)
+        {
+            rb2d.velocity = new Vector2(0, jumpingSpeed);
+            animator.SetBool("Jumping", true);
+            jumping = true;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            sit = true;
+            animator.SetBool("Sit", true);
+        } else
+        {
+            sit = false;
+            animator.SetBool("Sit", false);
+        }
+    }
+
+    void Move()
+    {
+        transform.Translate(inputVector * speed * Time.deltaTime);
+
     }
 }
