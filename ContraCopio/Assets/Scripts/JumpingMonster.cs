67404c043a33f51c4721e80b3b86f648e68b4771
Vesa Hovinen <46774886+welmuttaja@users.noreply.github.com>
2019-06-04 13:41:43 +0300

    Map
    Tileset + map

--- ContraCopio/Assets/Scripts/JumpingMonster.cs
+++ ContraCopio/Assets/Scripts/JumpingMonster.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterMove : MonoBehaviour
+public class JumpingMonster : MonoBehaviour
 {
 
     public float speed = 1f;
-    // Start is called before the first frame update
-    public GameObject enemy;
-    public GameObject player;
-    public GameObject bullet;
+
+    public float min = 0f;
+    public float max = 10f;
+    public float randomUp = 1f;
 
     void Start()
     {
@@ -20,6 +20,8 @@ public class MonsterMove : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.left *(speed * Time.deltaTime));
+        randomUp = Random.Range(min, max);
+        transform.Translate(Vector2.up * randomUp * Time.deltaTime);
     }
 
 }
