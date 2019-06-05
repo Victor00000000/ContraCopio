833cbf43f9ee9b806c7cabbc84a21f3b84ef7425
Viktor Haimi <viktor.i.haimi@gmail.com>
2019-06-05 12:37:31 +0300

    our animation
    anumaatio

--- ContraCopio/Assets/Scripts/PlayerScript.cs
+++ ContraCopio/Assets/Scripts/PlayerScript.cs
@@ -11,9 +11,9 @@ public class PlayerScript : MonoBehaviour
     public float jumpingSpeed = 5;
     Rigidbody2D rb2d;
     Animator animator;
-    public bool jumping = false;
+    bool jumping = false;
     bool collision = true;
-    public float fallingTimer;
+    float fallingTimer;
     public Transform groundCheck;
 
     // Fireing
@@ -34,9 +34,10 @@ public class PlayerScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f, LayerMask.NameToLayer("Ground"));
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
         if (colliders != null)
         {
+            Debug.Log("collider not null");
             foreach(Collider2D col in colliders)
             {
                 if (col.gameObject != gameObject)
@@ -76,11 +77,11 @@ public class PlayerScript : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             inputVector.x = -1;
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(-1, 1, 1);
         } else if (Input.GetKey(KeyCode.D))
         {
             inputVector.x = 1;
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(1, 1, 1);
         }
 
         if (Input.GetKeyDown(KeyCode.Y) && jumping == false)
@@ -94,8 +95,7 @@ public class PlayerScript : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Y))
             {
-                Debug.Log("here");
-                fallingTimer = 10f;
+                fallingTimer = 0.5f;
                 Physics2D.IgnoreLayerCollision(0, 8);
                 rb2d.velocity = Vector2.zero;
             }
@@ -107,7 +107,8 @@ public class PlayerScript : MonoBehaviour
 
         // Fireing
         gunCooldownTimer -= Time.deltaTime;
-        if (Input.GetKey(KeyCode.G) && gunCooldownTimer < 0) Shoot();
+        if (Input.GetKey(KeyCode.G) && gunCooldownTimer < 0)
+            Shoot();
     }
 
     void Move()
@@ -134,23 +135,25 @@ public class PlayerScript : MonoBehaviour
             if (right) bul.SetDirection(Vector2.right + Vector2.up);
             else if (left) bul.SetDirection(Vector2.left + Vector2.up);
             else bul.SetDirection(Vector2.up);
-        } else if (transform.localScale.x < 0) { // If facing right
+        } else if (transform.localScale.x > 0) { // If facing right
             bul.SetDirection(Vector2.right);
         } else { // Else it's facing left
             go.GetComponent<BulletScript>().SetDirection(Vector2.left);
         }
     }
 
-
+    /*
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.collider.CompareTag("Ground")) {
             jumping = false;
             animator.SetBool("Jumping", false);
         } else if (other.collider.CompareTag("Death") ||
                   other.collider.CompareTag("Enemy")) {
-            if (collision) Die();
+            if (collision)
+                Die();
         }
     }
+    */
 
     void Die() {
         Vector2 newPos = new Vector2(-50000, 0);
@@ -177,4 +180,5 @@ public class PlayerScript : MonoBehaviour
         Gizmos.color = Color.red;  
         Gizmos.DrawWireSphere(groundCheck.position, 0.2f);
     }
+
 }
